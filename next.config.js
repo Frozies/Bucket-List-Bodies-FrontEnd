d3f3724261481f13b8fd0911a315504944472ca8
Frozies <52686922+Frozies@users.noreply.github.com>
2021-07-10 20:06:43 -0400

    SVG Fix again

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const withImages = require('next-images')
+
 module.exports = {
   reactStrictMode: true,
 
@@ -7,11 +9,13 @@ module.exports = {
     );
 
     config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ['@svgr/webpack'],
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000
+        }
+      }
     });
 
     return config;
