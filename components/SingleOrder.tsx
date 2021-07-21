98e8017a1e8b69f8b86b3d35ec70dbf449ed3b7e
Davin <52686922+Frozies@users.noreply.github.com>
2021-07-20 20:51:56 -0400

    colors

--- components/SingleOrder.tsx
+++ components/SingleOrder.tsx
@@ -1,7 +1,14 @@
 import {gql, useQuery} from "@apollo/client";
-import {Box, Container, Grid, Paper, TextField} from "@material-ui/core";
+import {Box, Container, Grid, Input, InputAdornment, Paper, TextField} from "@material-ui/core";
+import FormControl from "@material-ui/core/FormControl";
 import {makeStyles} from "@material-ui/styles";
 import {spacing} from "@material-ui/system";
+// @ts-ignore
+import MaskedInput from "react-text-mask";
+import PropTypes from "prop-types";
+import React from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+
 
 //Commented out temporarily input new commands here.
 /*const randData = gql`
@@ -44,6 +51,41 @@ const customerContactQuery = gql`
 
 `*/
 
+function TextMaskCustom(props: { [x: string]: any; inputRef: any; }) {
+    const { inputRef, ...other } = props;
+
+    return (
+        <MaskedInput
+            {...other}
+            ref={(ref: { inputElement: any; }) => {
+                inputRef(ref ? ref.inputElement : null);
+            }}
+            mask={[
+                "(",
+                /[1-9]/,
+                /\d/,
+                /\d/,
+                ")",
+                " ",
+                /\d/,
+                /\d/,
+                /\d/,
+                "-",
+                /\d/,
+                /\d/,
+                /\d/,
+                /\d/
+            ]}
+            placeholderChar={"\u2000"}
+            showMask
+        />
+    );
+}
+
+TextMaskCustom.propTypes = {
+    inputRef: PropTypes.func.isRequired
+};
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -51,24 +93,21 @@ const useStyles = makeStyles((theme) => ({
         flexWrap: 'wrap',
     },
     textField: {
-        marginLeft: 3,
-        marginRight: 3,
+        background: 'white',
+        border: 0,
+        borderRadius: 3,
+        margin: '0 5px 5px 0',
+
     },
     cardStyle: {
-        border: '0',
-        marginBottom: '30px',
-        marginTop: '30px',
-        borderRadius: '6px',
-        color: 'rgba(0, 0, 0, 0.87)',
-        background: '#ec0000',
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: '100%',
         width: '100%',
-        boxShadow: '0 1px 4px 0 rgba(0, 0, 0, 0.14)',
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        minWidth: '0',
-        wordWrap: 'break-word',
-        fontSize: '.875rem',
+        padding: '30px 30px',
     }
 }));
 
@@ -76,6 +115,19 @@ const useStyles = makeStyles((theme) => ({
 // */
 function CustomerContactInfo(customerId: Number) {
     const classes = useStyles();
+
+    //Phone Number Values
+    const [values, setValues] = React.useState({
+        textmask: "(239) 123-4567",
+    });
+
+    const handleChange = (event: { target: { name: any; value: any; }; }) => {
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value
+        });
+    };
+
     //write a new function here
     /*const {loading, error, data} = useQuery(customerContactQuery, {
         variables: {
@@ -88,106 +140,83 @@ function CustomerContactInfo(customerId: Number) {
     if(data) console.log(data)*/
 
     return (
-        <div>
-            <Box className={classes.cardStyle}>
+        <div className={classes.cardStyle}>
+            <div>
                 <h3>Address</h3>
-                <TextField
-                    margin={"normal"}
-                    id={"line1"}
-                    label={"Address Line 1"}
-                    variant={"outlined"}
-                    fullWidth={true}
-                    className={classes.textField}
-                />
+                <form>
+                    <TextField
+                        id={"line1"}
+                        label={"Address Line 1"}
+                        variant={"outlined"}
+                        fullWidth={true}
+                        className={classes.textField}
+                    />
 
-                <TextField
-                    margin={"normal"}
-                    id={"line2"}
-                    label={"Address Line 2"}
-                    variant={"outlined"}
-                    fullWidth={true}
-                    className={classes.textField}
-                />
+                    <TextField
+                        id={"line2"}
+                        label={"Address Line 2"}
+                        fullWidth={true}
+                        variant={"outlined"}
+                        className={classes.textField}
+                    />
 
-                <TextField
-                    margin={"normal"}
-                    id={"zip"}
-                    label={"Postal Code"}
-                    variant={"outlined"}
-                    className={classes.textField}
-                />
+                    <TextField
+                        id={"zip"}
+                        label={"Postal Code"}
+                        variant={"outlined"}
+                        placeholder={"33919"}
+                        className={classes.textField}
+                    />
 
-                <TextField
-                    margin={"normal"}
-                    id={"city"}
-                    label={"City"}
-                    variant={"outlined"}
-                    className={classes.textField}
-                />
+                    <TextField
+                        id={"city"}
+                        label={"City"}
+                        variant={"outlined"}
+                        placeholder={"New York"}
+                        className={classes.textField}
+                    />
+                </form>
 
+            </div>
+            <div>
                 <h3>Personal</h3>
+                <form>
+                    <TextField
+                        id={"email"}
+                        label={"Email"}
+                        variant={"outlined"}
+                        placeholder={"JohnWick@hotmail.com"}
+                        className={classes.textField}
+                    />
 
-                <TextField
-                    margin={"normal"}
-                    id={"email"}
-                    label={"Email"}
-                    variant={"outlined"}
-                    className={classes.textField}
-                />
+
+                    <TextField
+                        id={"Name"}
+                        label={"Name"}
+                        variant={"outlined"}
+                        placeholder={"John Wick"}
+                        className={classes.textField}
+                    />
+
+                    <TextField
+                        placeholder={values.textmask}
+                        onChange={handleChange}
+                        label="Phone Number"
+                        id="phone"
+                        variant={"outlined"}
+                        className={classes.textField}
+                    />
 
 
-                <TextField
-                    margin={"normal"}
-                    id={"Name"}
-                    label={"Name"}
-                    variant={"outlined"}
-                    className={classes.textField}
-                />
-
-                <TextField
-                    margin={"normal"}
-                    id={"phone"}
-                    label={"Phone"}
-                    variant={"outlined"}
-                    className={classes.textField}
-                />
-            </Box>
+                </form>
+            </div>
         </div>
     )
 
-    // return data.customer.map((data: any)=> (
-    //     <div key={data.id}>
-    //         <h3>{data.name}</h3>
-    //         <p>{data.phone}</p>
-    //         <p>{data.address.line1}</p>
-    //         <p>{data.address.city}</p>
-    //         <p>{data.address.postal}</p>
-    //     </div>
-    // ));
 };
 
 //todo: add sources for specific order information. maybe even add a facade after you get a few functions.
 //Todo: pass data as is and let components handle the stylizing.
 //todo: Meals, orders, users crud
-
-// function RandomData() {
-//     const {loading, error, data} = useQuery(randData);
-//     if (loading) return <p>Loading...</p>;
-//     if (error) return <p>Error :({console.log(error)}</p> ;
-//     if (!data) return <p>No Data!</p>
-//
-//     return data.orders.map((data: any)=> (
-//         <div key={data.id}>
-//             <h3>{data.customer.name}</h3>
-//             <p>{data.customer.phone}</p>
-//             <p>{data.customer.address.line1}</p>
-//             <p>{data.customer.address.city}</p>
-//             <p>{data.customer.address.postal}</p>
-//             <p>{data.meals.map((mealData: any) => (
-//                 <h1 key={mealData.id}>{mealData.protein}</h1>
-//             ))}</p>
-//         </div>
-//     ));
-// };
 
 module.exports = CustomerContactInfo;
