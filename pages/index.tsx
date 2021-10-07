781fc7ce613b454e27986227f3961eae3e1639de
Davin <52686922+Frozies@users.noreply.github.com>
2021-10-07 18:29:38 -0400

    Adjusted Hero Banner added github deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,6 @@ import TopAppBar from "../components/Admin/Util/TopAppBar";
 import React from "react";
 import {Helmet} from "react-helmet-async";
 import styles from '../styles/Home.module.css'
-import Image from 'next/image'
-
-/*todo:
-            -   Header -- finish buttons and stuff
-            -   Mobile Styling -- css hell
-            -   Multi page routing -- express?
-            -   Footer -- app bar?
-            */
-
-const outcomes = [
-    "How to build this landing page with Next.js",
-    "How to create API endpoint and integrate with ConvertKit API",
-    "How to use React Hook Form and TailwindCSS",
-];
-
-const ComingSoonBadge = () => (
-    <span className="bg-blue-500 text-white text-xs py-1 px-2 rounded-md mb-4 inline-block">
-    Coming Soon!
-  </span>
-);
 
 export default function Home() {
     const pageTitle = "Bucket List Bodies"
@@ -31,6 +11,9 @@ export default function Home() {
                 <title>{pageTitle}</title>
                 <meta name="description" content="Bucket list bodies is a good place to get food from!" />
                 <link rel="icon" href="/favicon.ico" />
+                <style>
+                    @import url('https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap');
+                </style>
             </Helmet>
 
             {/*<TopAppBar pageTitle={pageTitle} />*/}
@@ -40,14 +23,23 @@ export default function Home() {
                 backgroundImage: 'url(/shutterstock/hero.jpg)'
             }}>
                 <div className={styles.heroOverlay}>
-                    <h1 >Bucket list bodies is coming soon!</h1>
+                    <h1>Bucket list bodies is coming soon!</h1>
+                    {/*TODO:
+                        Instagram: https://www.instagram.com/bucket_list_body/
+                        Email:
+                        Phone:
+                        Current Menu:
+                        Instagram Gallery:
+
+                    */}
+
                 </div>
             </div>
 
-            <main className={styles.container}>
+            {/*<main className={styles.container}>
                 <div className={styles.main}>
                 </div>
-            </main>
+            </main>*/}
 
             {/*<footer className={styles.footer}>
                 // Hello World!
